be48dc43ab979c291e8a0544be729f420d01c7fa
Leovpf <leovpf@hotmail.com>
2025-09-16 17:28:59 -0300

    inserção do Index e atualizações diversas

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// const router = require("./routes/router");
 const bodyParser = require("body-parser");
 const express = require("express");
 const cors = require("cors");
@@ -9,7 +8,22 @@ app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cors({ credentials: true, origin: true }));
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(express.json());
-// app.use(router);
+
+const bookingsRouter = require('./routes/bookings');
+const categoriesRouter = require('./routes/categories');
+const customersRouter = require('./routes/customers');
+const hotelsRouter = require('./routes/hotels');
+const metadataRouter = require('./routes/metadata');
+const paymentsRouter = require('./routes/payments');
+const roomsRouter = require('./routes/rooms');
+
+app.use('/bookings', bookingsRouter);
+app.use('/categories', categoriesRouter);
+app.use('/customers', customersRouter);
+app.use('/hotels', hotelsRouter);
+app.use('/metadata', metadataRouter);
+app.use('/payments', paymentsRouter);
+app.use('/rooms', roomsRouter);
 
 app.listen(process.env.PORT, () => {
     console.log('_____Start_____')
